3d663a12a43da3c61793311599004ff02365f403
ska034 <al.ig.erofeev@mail.ru>
2022-08-07 01:41:45 +0300

    First addition

---
+++ config_example.py
@@ -0,0 +1,1 @@
+db_connection_string = 'postgresql+psycopg2://user:password@datebase_address'
---
+++ models/office.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class Office(db.Model):
+    __tablename__ = 'offices'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"\nId: {self.id}\nTitle: {self.title}\n Address: {self.address}"
